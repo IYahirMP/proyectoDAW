c735c1b6790a93994c103a80194b4a571191cbca
Ivan Yahir Mojica Pineda <ivanyahirmopi@gmail.com>
2023-03-21 23:37:48 -0600

    Primer commit. Credenciales DB agregadas.

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+class Database
+{
+    public static function connect()
+    {
+        $db = new mysqli("localhost", "ivan_daw", "4/YYS2Q.dMsXRtk0", "proyecto_daw");
+        $db->query("SET NAMES 'uf8'");
+        return $db;
+    }
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//Se incluye el método que carga controladores controllers_autoload
+require_once "autoload.php";
+
+//Si está definidio el parámetro controller en la URL
+if (isset($_GET["controller"])) {
+    //El nombre del controlador será <controlador>Controller
+    $nombre_controlador = $_GET("controller") . "Controller";
+} else {
+    //Si no existe el parámetro controller, entonces se termina la ejecución
+    echo "La pagina que buscas no existe";
+    exit();
+}
+
+//Pasada la prueba de existencia del parámetro
+//Se revisa si existe la clase del controlador
+if (class_exists($nombre_controlador)) {
+    $controlador = new $nombre_controlador();
+
+    if (isset($_GET["action"]) && method_exists($controlador, $_GET["action"])) {
+        $action = $_GET("action");
+        $controlador->$action();
+    } else {
+        echo "La pagina que buscas no existe";
+    }
+} else {
+    echo "La pagina que buscas no existe";
+}
